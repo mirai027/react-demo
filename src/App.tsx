8aad11ce5042ddb202ec3d7d3f08cf500e3bffbc
mirai_white <mirai_white@foxmail.com>
2022-07-13 01:03:44 +0800

    azhe

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import logo from './logo.svg'
 import './App.scss'
 
 import * as THREE from 'three'
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
+import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader"
 
 const App = () => {
 
@@ -13,7 +15,10 @@ const App = () => {
     const scene = new THREE.Scene()
 
     const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2)
-    const material = new THREE.MeshNormalMaterial({})
+
+    const material = new THREE.MeshLambertMaterial({
+      color: 0xff0000
+    })
 
     const mesh = new THREE.Mesh(geometry, material)
     scene.add(mesh)
@@ -22,8 +27,38 @@ const App = () => {
     point.position.set(400, 200, 300)
     scene.add(point)
 
-    const ambient = new THREE.AmbientLight(0xfff)
+    const ambient = new THREE.AmbientLight(0x404040)
     scene.add(ambient)
+
+    const loader = new OBJLoader();
+
+    // load a resource
+    loader.load(
+      // resource URL
+      new URL('./azhe.obj', import.meta.url).href,
+      // called when resource is loaded
+      function (object) {
+
+        scene.add(object);
+
+      },
+      // called when loading is in progresses
+      function (xhr) {
+
+        console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+
+      },
+      // called when loading has errors
+      function (error) {
+
+        console.log('An error happened');
+
+      }
+    );
+
+
+    const axesHelper = new THREE.AxesHelper(5)
+    scene.add(axesHelper)
 
     const renderer = new THREE.WebGLRenderer({ antialias: true })
     renderer.setSize(window.innerWidth, window.innerHeight)
@@ -37,6 +72,10 @@ const App = () => {
       renderer.render(scene, camera)
     }
 
+    const controls = new OrbitControls(camera, renderer.domElement)
+    controls.addEventListener('change', () => {
+      renderer.render(scene, camera)
+    })
   }
 
   useEffect(() => {
