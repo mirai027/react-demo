37b00fb0c62e684d669757f0d3a128745011dfaf
yeyulin <yeyulin@qwk.onex.email>
2022-07-15 18:31:21 +0800

    skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -27,12 +27,7 @@ const App = () => {
   let numAnimations: number
 
   const allActions: THREE.AnimationAction[] = []
-  let currentBaseAction = 'idle'
-  const baseActions: ActionsType = {
-    idle: { weight: 1 },
-    walk: { weight: 0 },
-    run: { weight: 0 }
-  }
+
   const additiveActions: ActionsType = {
     sneak_pose: { weight: 0 },
     sad_pose: { weight: 0 },
@@ -42,8 +37,6 @@ const App = () => {
 
   let stats: Stats
   let panelSettings: Record<string, number | (() => void)>
-
-  const crossFadeControls: any[]= []
 
   function init() {
     camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 100)
@@ -94,14 +87,7 @@ const App = () => {
         let clip = animations[i]
         const name = clip.name
 
-        if (baseActions[name]) {
-
-          const action = mixer.clipAction(clip)
-          activateAction(action)
-          baseActions[name].action = action
-          allActions.push(action)
-
-        } else if (additiveActions[name]) {
+        if (additiveActions[name]) {
 
           // Make the clip additive and remove the reference frame
 
@@ -160,7 +146,7 @@ const App = () => {
 
   function activateAction(action: THREE.AnimationAction) {
     const clip = action.getClip()
-    const settings = baseActions[clip.name] || additiveActions[clip.name]
+    const settings = additiveActions[clip.name]
     setWeight(action, settings.weight)
     action.play()
   }
@@ -179,185 +165,26 @@ const App = () => {
 
   function createPanel() {
     const panel = new GUI({ width: 310 })
-    // const folder1 = panel.addFolder('Base Actions')
-    const folder2 = panel.addFolder('Additive Action Weights')
-    const folder3 = panel.addFolder('General Speed')
+    const folder = panel.addFolder('Additive Action Weights')
 
-    panelSettings = {
-      'modify time scale': 1.0
-    }
-
-    const baseNames = ['None', ...Object.keys(baseActions)]
-
-    for (let i = 0, l = baseNames.length; i !== l; ++i) {
-
-      const name = baseNames[i];
-      const settings = baseActions[name];
-      panelSettings[name] = function () {
-
-        const currentSettings = baseActions[currentBaseAction];
-        const currentAction = currentSettings ? currentSettings.action : null;
-        const action = settings ? settings.action : null;
-
-        if (currentAction !== action) {
-
-          prepareCrossFade(currentAction, action, 0.35);
-
-        }
-
-      };
-
-      // crossFadeControls.push(folder1.add(panelSettings, name));
-
-    }
+    panelSettings = {}
 
     for (const name of Object.keys(additiveActions)) {
 
       const settings = additiveActions[name];
 
       panelSettings[name] = settings.weight;
-      folder2.add(panelSettings, name, 0.0, 1.0, 0.01).listen().onChange(function (weight: number) {
 
-        setWeight(settings.action!, weight);
-        settings.weight = weight;
+      folder.add(panelSettings, name, 0.0, 4.0, 0.01).listen().onChange(function (weight: number) {
 
-      });
+        setWeight(settings.action!, weight)
+        settings.weight = weight
 
-    }
-
-    folder3.add(panelSettings, 'modify time scale', 0.0, 1.5, 0.01).onChange(modifyTimeScale);
-
-    // folder1.open();
-    folder2.open();
-    folder3.open();
-
-    crossFadeControls.forEach(function (control) {
-
-      control.setInactive = function () {
-
-        control.domElement.classList.add('control-inactive');
-
-      };
-
-      control.setActive = function () {
-
-        control.domElement.classList.remove('control-inactive');
-
-      };
-
-      const settings = baseActions[control.property];
-
-      if (!settings || !settings.weight) {
-
-        control.setInactive();
-
-      }
-
-    });
-
-  }
-
-  function modifyTimeScale(speed: number) {
-    mixer.timeScale = speed;
-  }
-
-
-  function prepareCrossFade(startAction: any, endAction: any, duration: any) {
-
-    // If the current action is 'idle', execute the crossfade immediately;
-    // else wait until the current action has finished its current loop
-
-    if (currentBaseAction === 'idle' || !startAction || !endAction) {
-
-      executeCrossFade(startAction, endAction, duration);
-
-    } else {
-
-      synchronizeCrossFade(startAction, endAction, duration);
+      })
 
     }
 
-    // Update control colors
-
-    if (endAction) {
-
-      const clip = endAction.getClip();
-      currentBaseAction = clip.name;
-
-    } else {
-
-      currentBaseAction = 'None';
-
-    }
-
-    crossFadeControls.forEach(function (control) {
-
-      const name = control.property;
-
-      if (name === currentBaseAction) {
-
-        control.setActive();
-
-      } else {
-
-        control.setInactive();
-
-      }
-
-    });
-
-  }
-
-  function executeCrossFade(startAction: any, endAction: any, duration: any) {
-
-    // Not only the start action, but also the end action must get a weight of 1 before fading
-    // (concerning the start action this is already guaranteed in this place)
-
-    if (endAction) {
-
-      setWeight(endAction, 1);
-      endAction.time = 0;
-
-      if (startAction) {
-
-        // Crossfade with warping
-
-        startAction.crossFadeTo(endAction, duration, true);
-
-      } else {
-
-        // Fade in
-
-        endAction.fadeIn(duration);
-
-      }
-
-    } else {
-
-      // Fade out
-
-      startAction.fadeOut(duration);
-
-    }
-
-  }
-
-  function synchronizeCrossFade(startAction: any, endAction: any, duration: any) {
-
-    mixer.addEventListener('loop', onLoopFinished);
-
-    function onLoopFinished(event: any) {
-
-      if (event.action === startAction) {
-
-        mixer.removeEventListener('loop', onLoopFinished);
-
-        executeCrossFade(startAction, endAction, duration);
-
-      }
-
-    }
-
+    folder.open();
   }
 
   function animate() {
@@ -365,15 +192,6 @@ const App = () => {
     // Render loop
 
     requestAnimationFrame(animate);
-
-    for (let i = 0; i !== numAnimations; ++i) {
-
-      const action = allActions[i];
-      const clip = action.getClip();
-      const settings = baseActions[clip.name] || additiveActions[clip.name];
-      settings.weight = action.getEffectiveWeight();
-
-    }
 
     // Get the time elapsed since the last frame, used for mixer update
 
